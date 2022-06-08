34066c2db04862393099d10cc8fb5cd82a7b63bf
MrBirdman1991 <birdman-coding@outlook.de>
2022-06-08 19:10:25 +0200

    starter

---
+++ starter/src/util/numbers.test.js
@@ -0,0 +1,41 @@
+import { describe, it, expect } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
+
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input1 = "invalid";
+    const input2 = {};
+
+    const result1 = transformToNumber(input1);
+    const result2 = transformToNumber(input2);
+
+    expect(result1).toBeNaN();
+    expect(result2).toBeNaN();
+  });
+});
+
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2"];
+
+    const result = cleanNumbers(numberValues);
+
+    expect(result).toEqual([1, 2]);
+  });
+
+  it("it should throw an error if an array with at least one empty string is provided", () => {
+    const numberValues = ["1", ""];
+
+    const cleanFn = () => cleanNumbers(numberValues);
+
+    expect(cleanFn).toThrow();
+  });
+});
